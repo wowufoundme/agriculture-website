82065c4a70524c76b2ee9ed61c780fd321f76d43
Mohit Nagpal <nagpalm7@gmail.com>
2019-09-22 15:51:31 +0530

    village add delete and edit

--- js/ado.js
+++ js/ado.js
@@ -1,3 +1,83 @@
+// get data for specified page
+function getData(page = 1){
+    if (page !== 1)
+        url = `http://13.235.100.235:8000/api/users-list/ado/?page=${page}`
+    else
+        url = `http://13.235.100.235:8000/api/users-list/ado/`
+    $.ajax({
+        url: url,
+        type: 'GET',
+        headers: {
+            'Authorization': 'Token a5ed9f187e22c861262a5e5a37eaed92a6c84c0c'
+        },
+        async: false,
+        dataType: 'json',
+        beforeSend: function() {
+            $(".pagination").html("");
+            $("tbody").html("");
+            $(".loading").show();
+        },
+        success: function(res) {
+            if (res.results.length > 0) {
+                res.results.map(item => {
+                	row = `
+                		<tr key=${item.id}>
+                            <td>${res.results.indexOf(item) + (page-1)*10 + 1}</td>
+                            <td id="title">${item.name}</td>
+                            <td id="village">${item.village.village}</td>
+                            <td id="number">${item.number}</td>
+                            <td id="email">${item.email}</td>
+                            <td>
+                                <a id="editbutton" class="btn waves-effect waves-light modal-trigger" data-target="editAdo"><i class="large material-icons left">edit</i>Edit</a>
+                                <a id="deletebutton" class="btn red waves-effect modal-trigger" data-target="deleteAdo"><i class="material-icons left">delete</i>DELETE</a>
+                            </td>
+                        </tr>
+                	`
+                    $('tbody').append(row)
+                })
+                arrow_left_enabled = 
+                `
+                    <li id="left" class="waves-effect">
+                        <a href="#!">
+                            <i class="material-icons">chevron_left</i>
+                        </a>
+                    </li>
+                `
+                if(page!==1){
+                    $('.pagination').append(arrow_left_enabled);
+                }
+                for(var i=0;i<res.count/10;i++){
+                    page_tab = `<li id="page-tab" class="waves-effect"><a href="#!">${i+1}</a></li>`
+                    active_tab = `<li id="page-tab" class="active"><a href="#!">${i+1}</a></li>`
+                    if(i===page-1){
+                        $('.pagination').append(active_tab);
+                    }else{
+                        $('.pagination').append(page_tab);
+                    }
+                }
+
+                if(res.count > 10 && page - 1 !== parseInt(res.count/10)){
+                    arrow_right = `
+                        <li id="right" class="waves-effect">
+                            <a href="#!">
+                                <i class="material-icons">chevron_right</i>
+                            </a>
+                        </li>
+                    `
+                    $('.pagination').append(arrow_right)
+                }
+
+            } else {
+                $('table').after('<center>No Data Available</center>')
+            }
+            $(".loading").hide();
+        },
+        error: function(e) {
+            console.log(e);
+        }
+    });
+}
+
 $(document).ready(function() {
     $('.modaladd').modal();
     $('.modaledit').modal();
@@ -5,4 +85,5 @@ $(document).ready(function() {
     $(document).ready(function() {
         $('select').formSelect();
     });
+    getData();
 });
