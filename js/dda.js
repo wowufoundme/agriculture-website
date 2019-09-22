0ec61cd5f19c8bea43f352f4330308c6e9fbd376
harsh gandhi <harshgandhi043@gmail.com>
2019-09-22 12:06:25 +0530

    added dropdown

--- js/dda.js
+++ js/dda.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $('.modaladd').modal();
     $('.modaledit').modal();
     $('.modaldelete').modal();
+    $(document).ready(function() {
+        $('select').formSelect();
+    });
 });
