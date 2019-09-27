e7f624fd73b69e351d6809b46ba9897f73a59176
harsh gandhi <harshgandhi043@gmail.com>
2019-09-27 18:39:36 +0530

    made login and register responsive

--- js/common.js
+++ js/common.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
     if ($(window).width() < 600) {
 
         // $('a').remove();
-        $('#addbulkbtnid').html('<a id="addbulkbutton" class="right waves-effect blue lighten-1 waves-light btn modal-trigger" data-target="addBulk">AddBulk</a>')
-        $('#addbtnid').html('<a id="addbutton" class="right waves-effect blue lighten-1 waves-light btn modal-trigger" data-target="addDistrict">Add</a>')
-        console.log("width less than 600");
+        // $('#addbulkbtnid').html('<a id="addbulkbutton" class="left waves-effect blue lighten-1 waves-light btn modal-trigger" data-target="addBulk">AddBulk</a>')
+        // $('#addbtnid').html('<a id="addbutton" class="left waves-effect blue lighten-1 waves-light btn modal-trigger" data-target="addDistrict">Add</a>')
+        // console.log("width less than 600");
     }
 })
